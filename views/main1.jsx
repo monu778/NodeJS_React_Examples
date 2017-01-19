d8435fa43fa810dca3801b5d6c4eb832f94f8eea
monu778 <sravanreddy2611@gmail.com>
2017-01-19 19:36:59 +0530

    Create main1.jsx

---
+++ views/main1.jsx
@@ -0,0 +1,45 @@
+/**
+ * Created by sravan on 17/01/17.
+ */
+
+
+import Line from './Line';
+import Circle from './Circle';
+//const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+
+class CircleGraph extends React.Component {
+    render() {
+
+
+        return (<html>
+        <head>
+            <title>Circle Graph</title>
+            <link href="/javascripts/lib/bootstrap-css/css/bootstrap.min.css" rel="stylesheet"></link>
+            <script src="./lib/jquery/dist/jquery.min.js"></script>
+        </head>
+        <body>
+        <div className="panel panel-default">
+            <div className="panel-heading">
+                Showing graph
+            </div>
+            <div className="panel-body">
+             <div><Circle strokeWidth="2" percent="58" /></div>
+                <div><Circle strokeWidth="2" percent="8" /></div>
+                <div><Circle strokeWidth="2" percent="12" /></div>
+            </div></div>
+        <script dangerouslySetInnerHTML={{__html: `
+
+`}} />
+        </body>
+            </html>
+                );
+
+    }
+}
+
+
+export default CircleGraph;
