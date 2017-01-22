495b6559468b18e1c15101eab36884223826dcd2
monu778 <sravanreddy2611@gmail.com>
2017-01-22 19:22:30 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,44 @@
 var gulp = require("gulp");
-var browserify = require("browserify");
+var browserify = require("gulp-browserify");
 var reactify = require("reactify");
+var babelify = require("babelify");
 var source = require("vinyl-source-stream");
-
+var watchify = require("watchify");
+var babel = require('gulp-babel');
 gulp.task("bundle", function () {
-    return browserify({
-        entries: "./public/javascripts/main.jsx",
+
+    gulp.src('./build/public/main1.js')
+        .pipe(browserify({
+            insertGlobals : true,
+            debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('./build/js'))
+
+   /* return browserify({
+        entries: "./build/public/main1.js",
         debug: true
-    }).transform(reactify)
+    }).transform(babelify)
         .bundle()
-        .pipe(source("main.js"))
-        .pipe(gulp.dest("public"))
+        .pipe(source("m.js"))
+        .pipe(gulp.dest("build/public")) */
 });
+
+gulp.task('reacti', function () {
+    var bundler = watchify(browserify(watchify.args));
+    console.log("===");
+    return bundler.add('./public/circle.jsx')
+        .transform('reactify',{harmony:true, es6module:true})
+        .bundle()
+        .pipe(source('circle.js'))
+        .pipe(gulp.dest('public'));
+
+});
+gulp.task("jsx", function(){
+    return gulp.src("/*.jsx")
+        .pipe(babel({
+            presets: ['es2015',"react"]
+        }))
+        .pipe(gulp.dest("public"));
+});
+
+
